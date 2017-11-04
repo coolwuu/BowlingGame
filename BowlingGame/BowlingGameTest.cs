10ae0aed57cc8eee7e44cb5d4a954460d8fbbccd
coolwuu <coolwuu0207@gmail.com>
2017-11-04 11:03:04 +0800

    Add Nunit reference Add fail test :: All 0 point should return final score correctly.

---
+++ BowlingGame/BowlingGameTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace BowlingGame
+{
+    public class BowlingGameTest
+    {
+        [Test]
+        public void all_0_point_should_return_score_correctly()
+        {
+            var bowlingGame = new BowlingGame();
+            string result = "0- 0- 0- 0- 0- 0- 0- 0- 0- 0-";
+
+            var actual = bowlingGame.FinalScoreByResult(result);
+
+            Assert.AreEqual(0,actual);
+        }
+
+        public class BowlingGame
+        {
+            public int FinalScoreByResult(string result)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}

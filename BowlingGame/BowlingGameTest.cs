11ecef13b4cbe4674ed0b71d949b21b39fec4486
coolwuu <coolwuu0207@gmail.com>
2017-11-04 11:09:36 +0800

    [R] rename test method add fail test :: get 1 pin every frame should return final score correctly

--- BowlingGame/BowlingGameTest.cs
+++ BowlingGame/BowlingGameTest.cs
@@ -7,13 +7,23 @@ namespace BowlingGame
         public BowlingGame Game = new BowlingGame();
 
         [Test]
-        public void all_0_point_should_return_score_correctly()
+        public void get_0_pin_every_frame_should_return_score_correctly()
         {
             
             string result = "0- 0- 0- 0- 0- 0- 0- 0- 0- 0-";
 
             var actual = Game.FinalScoreByResult(result);
             Assert.AreEqual(0,actual);
+        }
+
+        [Test]
+        public void get_1_pin_every_frame_should_return_score_correctly()
+        {
+
+            string result = "1- 1- 1- 1- 1- 1- 1- 1- 1- 1-";
+
+            var actual = Game.FinalScoreByResult(result);
+            Assert.AreEqual(1, actual);
         }
 
         public class BowlingGame

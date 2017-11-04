422a8abff571f84e8f28273dc6597861cc88ba93
coolwuu <coolwuu0207@gmail.com>
2017-11-04 14:54:17 +0800

    [R] reduce complexity of methods

--- BowlingGame/BowlingGame.cs
+++ BowlingGame/BowlingGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BowlingGame
 {
@@ -6,6 +7,7 @@ namespace BowlingGame
     {
         public class BowlingGame
         {
+
             public int FinalScoreByResult(string result)
             {
                 int finalScore = 0;
@@ -13,47 +15,74 @@ namespace BowlingGame
 
                 for (var i = 0; i < pinResults.Length; i++)
                 {
-                    if (i > 8)
+                    if (IsLastFrame(i))
                     {
-                        if (isStrike(pinResults[i]))
-                        {
-                            finalScore += GetScoreByFrame(pinResults[i]);
-                            continue;
-                        }
-                        if (isSpare(pinResults[i]))
-                        {
-                            
-                            finalScore += GetScoreByFrame(pinResults[i]) + int.Parse(pinResults[i][2].ToString());
-                            continue;
-                        }
-
-                        finalScore += GetScoreByFrame(pinResults[i]);
+                        finalScore += GetLastFrameScore(pinResults, i);
                     }
                     else
                     {
-                        if (isStrike(pinResults[i]))
-                        {
-                            finalScore += GetScoreByFrame(pinResults[i])
-                                          + GetNext2ThrowsScore(pinResults[i + 1] + " " + pinResults[i + 2]);
-                            continue;
-                        }
-                        if (isSpare(pinResults[i]))
-                        {
-                            if (isStrike(pinResults[i + 1]))
-                            {
-                                finalScore += GetScoreByFrame(pinResults[i]) + GetScoreByFrame(pinResults[i + 1]);
-                                continue;
-                            }
-                            finalScore += GetScoreByFrame(pinResults[i]) + int.Parse(pinResults[i + 1][0].ToString());
-                            continue;
-                        }
-                        finalScore += GetScoreByFrame(pinResults[i]);
+                        finalScore += GetFrameScore(pinResults, i);
                     }
                 }
 
                 return finalScore;
             }
 
+            private int GetFrameScore(string[] pinResults, int i)
+            {
+                int score = 0;
+                if (isStrike(pinResults[i]))
+                {
+                    score += GetScoreWhenStrike(pinResults, i);
+                    return score;
+                }
+                if (isSpare(pinResults[i]))
+                {
+                    score += GetScoreWhenSpare(pinResults, i);
+                    return score;
+                }
+                score += GetScoreByFrame(pinResults[i]);
+                return score;
+            }
+
+            private int GetLastFrameScore(string[] pinResults, int i)
+            {
+                int score = 0;
+                if (isStrike(pinResults[i]))
+                {
+                    score += GetScoreByFrame(pinResults[i]);
+                    return score;
+                }
+                if (isSpare(pinResults[i]))
+                {
+                    score += GetScoreByFrame(pinResults[i]) + int.Parse(pinResults[i][2].ToString());
+                    return score;
+                }
+
+                return GetScoreByFrame(pinResults[i]);
+                
+            }
+
+            private int GetScoreWhenStrike(string[] pinResults, int i)
+            {
+                return GetScoreByFrame(pinResults[i])
+                       + GetNext2ThrowsScore(pinResults[i + 1] + " " + pinResults[i + 2]);
+            }
+            private int GetScoreWhenSpare(string[] pinResults, int i)
+            {
+                int score = 0;
+                if (isStrike(pinResults[i + 1]))
+                {
+                    score += GetScoreByFrame(pinResults[i]) + GetScoreByFrame(pinResults[i + 1]);
+                    return score;
+                }
+                score += GetScoreByFrame(pinResults[i]) + int.Parse(pinResults[i + 1][0].ToString());
+                return score;
+            }
+            private static bool IsLastFrame(int i)
+            {
+                return i > 8;
+            }
             private int GetNext2ThrowsScore(string pinResults)
             {
                 string[] pinResult = pinResults.Split(' ');
@@ -65,7 +94,6 @@ namespace BowlingGame
                 }
                 return GetScoreByFrame(pinResult[0]);
             }
-
             private bool isStrike(string result)
             {
                 return result.Contains("X");
